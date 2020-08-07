ea4b670073f9f97015b3e8b07998b4ab05accc94
rafaelmspu2 <rafaelmspu2@gmail.com>
2020-08-07 00:17:56 -0300

    Update ep01.py

--- ep01.py
+++ ep01.py
@@ -1,37 +1,4 @@
 # coding=utf-8
-"""
-  AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP, 
-  DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESSE PROGRAMA. 
-  TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO PROGRAMA (EP) FORAM 
-  DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES
-  DESSE EP E QUE PORTANTO NÃO CONSTITUEM DESONESTIDADE ACADÊMICA
-  OU PLÁGIO.  
-  DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS AS CÓPIAS
-  DESSE PROGRAMA E QUE EU NÃO DISTRIBUI OU FACILITEI A
-  SUA DISTRIBUIÇÃO. ESTOU CIENTE QUE OS CASOS DE PLÁGIO E
-  DESONESTIDADE ACADÊMICA SERÃO TRATADOS SEGUNDO OS CRITÉRIOS
-  DIVULGADOS NA PÁGINA DA DISCIPLINA.
-  ENTENDO QUE EPS SEM ASSINATURA NÃO SERÃO CORRIGIDOS E,
-  AINDA ASSIM, PODERÃO SER PUNIDOS POR DESONESTIDADE ACADÊMICA.
-
-  Nome : Rafael Agra de Castro Motta
-  NUSP : 11807192
-  Turma: 7
-  Prof.: André Fujita
-
-  Referências: Com exceção das rotinas fornecidas no enunciado
-  e em sala de aula, caso você tenha utilizado alguma refência,
-  liste-as abaixo para que o seu programa não seja considerado
-  plágio ou irregular.
-  
-  Exemplo:
-  - O algoritmo Quicksort foi baseado em
-  http://wiki.python.org.br/QuickSort
-
-	[Seu programa]
-
-  """
-
 def main():
     # Pergunta ao usuário o modo
     modoOpera = int(input("modo:"))
